505c3905fcd7e61ea7d9d9e5d92a28810dd78677
AnastasiaZhdanova <anastasia.zhdanova.1995@gmail.com>
2019-01-14 11:09:09 +0200

    Initial Commit

---
+++ Screens/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Screens
+//
+//  Created by Anastasia Zhdanova on 1/14/19.
+//  Copyright © 2019 Anastasia Zhdanova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
