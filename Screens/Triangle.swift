f33c0dbfbcf7f7e02973df88fea00efe474cb34c
AnastasiaZhdanova <anastasia.zhdanova.1995@gmail.com>
2019-01-14 16:48:35 +0200

    layout screens

---
+++ Screens/Triangle.swift
@@ -0,0 +1,21 @@
+//
+//  Triangle.swift
+//  Screens
+//
+//  Created by Anastasia Zhdanova on 1/14/19.
+//  Copyright © 2019 Anastasia Zhdanova. All rights reserved.
+//
+
+import UIKit
+
+class Triangle: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
